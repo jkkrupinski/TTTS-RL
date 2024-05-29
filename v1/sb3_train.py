7d398c6a662bea1f762f26797035d7685d91791d
Jakub <jakub.k.krupinski@gmail.com>
2024-05-29 16:19:39 +0100

    Added saving/loading models

--- v1/sb3_train.py
+++ v1/sb3_train.py
@@ -4,11 +4,31 @@ import camera_env as camera_env
 from torch import nn
 from stable_baselines3 import DQN
 
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+checkpoint_callback = CheckpointCallback(
+    save_freq=20000,
+    save_path="./logs/",
+    name_prefix="rl_model",
+    save_replay_buffer=True,
+    save_vecnormalize=True,
+)
 
 env = gym.make("camera-v1", render_mode=None)
 
-model = DQN("CnnPolicy", env, verbose=1, buffer_size=1000)
-model.mnbv (total_timesteps=200_000, log_interval=100, progress_bar=True)
+# model = DQN("CnnPolicy", env, verbose=1, buffer_size=1000)
+
+model = DQN.load("logs/rl_model_60000_steps.zip")
+model.load_replay_buffer('logs/rl_model_replay_buffer_60000_steps.pkl')
+model.set_env(env)
+
+
+model.learn(
+    total_timesteps=200_000,
+    log_interval=100,
+    progress_bar=True,
+    callback=checkpoint_callback,
+)
 
 model.save("models/dqn_camera")
 
